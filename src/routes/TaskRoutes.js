15501294796e78b41c3650ce28099c200b8a43e3
GuzztMega <guzzt.mega@gmail.com>
2022-10-02 11:12:14 -0300

    Implementado endpoint GET ById

--- src/routes/TaskRoutes.js
+++ src/routes/TaskRoutes.js
@@ -5,8 +5,10 @@ const TaskController = require('../controller/TaskController');
 const TaskValidation = require('../middlewares/TaskValidation');
 const MacaddressValidation = require('../middlewares/MacaddressValidation');
 
+router.get('/:id', TaskController.get)
+router.get('/filter/all', MacaddressValidation, TaskController.getAll)
+
 router.post('/', TaskValidation, TaskController.create);
 router.put('/:id', TaskValidation, TaskController.update);
-router.get('/filter/all', MacaddressValidation, TaskController.getAll)
 
 module.exports = router;  
