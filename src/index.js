8bbf9cb957a3b3634741b33607da514c850b6a30
GuzztMega <guzzt.mega@gmail.com>
2022-11-06 11:10:40 -0300

    Added CORS library and altered server port

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 const express = require("express"); // importa a biblioteca express
+const cors = require("cors");
 const server = express(); //instancia o objeto express
 
+server.use(cors());
 server.use(express.json());
 
 const TaskRoutes = require('./routes/TaskRoutes');
 
 server.use('/task', TaskRoutes);
 
-server.listen(3000, () => {
+server.listen(8080, () => {
   console.log("ONLINE API...");
 });
 
